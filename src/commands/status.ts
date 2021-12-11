4eb689120ad04d009d19825caa5d39a12117eca0
kz-n <64893077+kz-n@users.noreply.github.com>
2021-12-11 05:33:35 +0100

    1.2.0 Separation

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -1,87 +1,8 @@
 import { SlashCommand } from "onion-lasers";
 import "../commons/embed";
 import { footer } from "../commons/embed";
-const fetch = require('node-fetch');
-
-/**
- * Format bytes as human-readable text.
- * 
- * @param bytes Number of bytes.
- * @param si True to use metric (SI) units, aka powers of 1000. False to use 
- *           binary (IEC), aka powers of 1024.
- * @param dp Number of decimal places to display.
- * 
- * @return Formatted string.
- */
- function humanFileSize(bytes: number, si=false, dp=1) {
-    const thresh = si ? 1000 : 1024;
-
-    if (Math.abs(bytes) < thresh) {
-      return bytes + ' B';
-    }
-
-    const units = si 
-      ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] 
-      : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
-    let u = -1;
-    const r = 10**dp;
-
-    do {
-      bytes /= thresh;
-      ++u;
-    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
-
-
-    return bytes.toFixed(dp) + ' ' + units[u];
-}
-
-async function getSpecificServer(identifier:string) {
-  let response = await fetch("https://mc.bloom.host/api/client/servers/"+identifier,{
-        method: "GET",
-        headers: {
-          "Accept": 'application/json',
-          "Content-Type": 'application/json',
-          "Authorization": 'Bearer '+process.env.PTERODACTYL_AUTH
-        }
-      }
-    );
-  return await response.json()
-}
-
-async function getSpecificServerResource(identifier:string) {
-  let response = await fetch("https://mc.bloom.host/api/client/servers/"+identifier+"/resources",{
-        method: "GET",
-        headers: {
-          "Accept": 'application/json',
-          "Content-Type": 'application/json',
-          "Authorization": 'Bearer '+process.env.PTERODACTYL_AUTH
-        }
-      }
-    );
-  return await response.json()
-}
-
-async function getAllServers() {
-  let response = await fetch("https://mc.bloom.host/api/client",{
-          method: "GET",
-          headers: {
-            "Accept": 'application/json',
-            "Content-Type": 'application/json',
-            "Authorization": 'Bearer '+process.env.PTERODACTYL_AUTH
-          }
-        }
-      );
-  
-  let available_servers:any = {}
-  let data = await response.json()
-  
-  for(let result of data.data) {
-    available_servers[result.attributes.name] = result.attributes.identifier
-  }
-  
-  
-  return available_servers;
-}
+import { bloomapi } from "../commons/bloom";
+import { usercommons } from "../commons/user";
 
 export default new SlashCommand({
   description: "Shows current status of Draelcraft",
@@ -94,15 +15,16 @@ export default new SlashCommand({
     }
   ],
   run: async (interaction: any) => {
+    let api = new bloomapi
+    let ucommons = new usercommons;
+
     let has_selected_specific_server = interaction.options.getString('server') !== null;
-    console.log(has_selected_specific_server);
-    
 
     if(has_selected_specific_server) {
       
       let selected_server = interaction.options.getString('server');
       
-      let all_servers = await getAllServers();
+      let all_servers = await api.getAllServers();
 
       let response_success = false;
 
@@ -117,9 +39,9 @@ export default new SlashCommand({
           response_success = true;
         
           let identifier = all_servers[selected_server]
-          const resource_data = await getSpecificServerResource(identifier)
+          const resource_data = await api.getSpecificServerResource(identifier)
       
-          const server_data = await getSpecificServer(identifier)
+          const server_data = await api.getSpecificServer(identifier)
           
           let color;
           if(resource_data.attributes.current_state === "running") {
@@ -136,7 +58,7 @@ export default new SlashCommand({
             fields: [
               {
                 name: "RAM",
-                value: humanFileSize(resource_data.attributes.resources.memory_bytes,true,2),
+                value: ucommons.humanFileSize(resource_data.attributes.resources.memory_bytes,true,2),
               },
               {
                 name: "CPU",
